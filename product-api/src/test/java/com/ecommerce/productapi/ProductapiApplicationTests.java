f9f923d60788ef1f4831d5eba3a0a2fa043f6454
Vishal Hulawale <hulawale.vishal@gmail.com>
2024-01-20 00:25:36 +0530

    product-api initial commit

---
+++ product-api/src/test/java/com/ecommerce/productapi/ProductapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.productapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
